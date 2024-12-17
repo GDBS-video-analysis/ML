c3ba82aca7037d6ab91dd8d8b138231260bd7493
MaisLaufen <vip.eftifanov@mail.ru>
2024-12-17 19:38:03 +0700

    Добавлена система очереди обработки со статусами

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,49 @@
+import asyncio
+from datetime import time
 from flask import Flask
+import threading
+from app.services.event_queue import event_queue
 from app.routes import video_bp
+from app.services.processing import process_event
+from app.services import insert_video_status, update_video_status
+
+async def process_event_background():
+    print("start processing thread.")
+    while True:
+        event_data = event_queue.get()
+        if event_data is None:
+            # Если очередь пустая
+            time.sleep(10)
+        else:
+            event_id, cur_status = event_data
+
+            print(f"Event with id={event_id} processing...")
+            try:
+                # Статус - в обработке
+                await update_video_status(analisys_id=cur_status, status=1)
+                await process_event(event_id)
+
+                # Статус - успешно обработано
+                await update_video_status(analisys_id=cur_status, status=2)
+            except Exception as e:
+                print(f"Error processing event {event_id}: {e}")
+                # Статус - ошибка обработки
+                await update_video_status(analisys_id=cur_status, status=3)
+
+def run_asyncio_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(process_event_background())
 
 def create_app():
     app = Flask(__name__)
-    
+
+    # Регистрируем blueprint для маршрутов
     app.register_blueprint(video_bp)
+
+    # Запуск фонового потока для обработки очереди
+    thread = threading.Thread(target=run_asyncio_loop)
+    thread.daemon = True
+    thread.start()
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,24 @@
+from app.services.event_queue import event_queue
 from flask import Blueprint, request, jsonify
-from app.services import Processing, insert_video_status, update_video_status
+from app.services import insert_video_status
 
 video_bp = Blueprint('video', __name__)
 
 @video_bp.route('/process_event', methods=['POST'])
-async def process_event():
+async def process_event_route():
     try:
-
         data = request.get_json()
         event_id = data.get('event_id')
 
         if not event_id:
-            return jsonify({"error": "event_id are required"}), 400
+            return jsonify({"error": "event_id is required"}), 400
+        
+        # Ставим в очередь
+        cur_status = await insert_video_status(event_id=event_id, status=0)
 
-        analisys_id = await insert_video_status(event_id, 0)
-        print(f"Record created with analisys_id: {analisys_id}")
+        event_queue.put((event_id, cur_status))
 
-        print("Start processing...")
-        processing = Processing(event_id)
-
-        try:
-            results = await processing.process_event()
-            await update_video_status(analisys_id, 1)
-            print("End processing successfully.")
-            return jsonify({"results": results}), 200
-
-        except Exception as e:
-            await update_video_status(analisys_id, 2)
-            print(f"Error during processing: {e}")
-            return jsonify({"error": "Processing failed", "details": str(e)}), 500
+        return jsonify({"message": "record added to queue"}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
